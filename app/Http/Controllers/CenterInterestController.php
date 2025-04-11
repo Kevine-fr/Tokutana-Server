8ef4a36b7b35293218ed345a5fd0997569af1561
Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>
2025-04-11 09:17:38 +0200

    Mise en Production

--- app/Http/Controllers/CenterInterestController.php
+++ app/Http/Controllers/CenterInterestController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\CenterInterest;
+
 class CenterInterestController extends Controller
 {
-    //
+     public function CenterInterests(){
+        try {
+            $CI = CenterInterest::latest()->take(15)->get();
+
+            if($CI->isEmpty()) {
+                return response()->json([
+                    'message' => "Aucune relation trouvée !",
+                ], 404);
+            }
+            return response()->json([
+                'message' => "Centres d'intérets récupérées avec succès !",
+                'data' => $CI,
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => "Erreur lors de la récupération des relations !",
+                'errors' => $th->getMessage()
+            ], 500);
+        }
+    }
 }
